9cc6400dd4a0c43ad5f8d3966d332ccde76e5ca6
Dennis Schenkel <dennisschenkel@googlemail.com>
2024-09-16 13:42:59 +0200

    MS-3 Add link to tool in list

--- src/components/tools/ToolsListItem.jsx
+++ src/components/tools/ToolsListItem.jsx
@@ -19,33 +19,35 @@ export default function ToolsListItem({tool}) {
     const icon = tool.icon.toLowerCase();
 
     return (
-        <div className={`${styles["list-item"]} row g-0`}>
-            <div className={`${styles["list-item-icon"]} col-1`}>
-                <div id="tool-icon">
-                    <Emoji unified={icon} size={40} />                    
+        <a href={`/tools/${tool.slug}`}>
+            <div className={`${styles["list-item"]} row g-0`}>
+                <div className={`${styles["list-item-icon"]} col-1`}>
+                    <div id="tool-icon">
+                        <Emoji unified={icon} size={40} />                    
+                    </div>
+                </div>  
+                <div className={`${styles["list-item-desc"]} col-9 ps-2`}>
+                    <div>
+                        <h3>{tool.title}</h3>
+                        <div className={`${styles["list-item-desc-text"]}`}>{tool.short_description}</div>
+                        <div className={`${styles["list-item-desc-misc"]}`}>
+                            <FontAwesomeIcon icon={faComment} /> 62<span> &#183; </span>
+                            {topicsList}<span> &#183; </span>{tool.profile.first_name} {tool.profile.last_name}<span> &#183; </span>{tool.created}
+                        </div>
+                    </div>
                 </div>
-            </div>  
-            <div className={`${styles["list-item-desc"]} col-9 ps-2`}>
-                <div>
-                    <h3>{tool.title}</h3>
-                    <div className={`${styles["list-item-desc-text"]}`}>{tool.short_description}</div>
-                    <div className={`${styles["list-item-desc-misc"]}`}>
-                        <FontAwesomeIcon icon={faComment} /> 62<span> &#183; </span>
-                        {topicsList}<span> &#183; </span>{tool.profile.first_name} {tool.profile.last_name}<span> &#183; </span>{tool.created}
+                <div className={`${styles["list-item-vote-container"]} col-2`}>
+                    <div className={`${styles["list-item-vote"]} justify-content-center`}>
+                        <div>
+                            <FontAwesomeIcon icon={faCaretUp} className="fa-xl" />
+                        </div>
+                        <div>
+                            {tool.vote_count}
+                        </div>
                     </div>
                 </div>
             </div>
-            <div className={`${styles["list-item-vote-container"]} col-2`}>
-                <div className={`${styles["list-item-vote"]} justify-content-center`}>
-                    <div>
-                        <FontAwesomeIcon icon={faCaretUp} className="fa-2x" />
-                    </div>
-                    <div>
-                        {tool.vote_count}
-                    </div>
-                </div>
-            </div>
-        </div>
+        </a>
 
     )
 }
--- src/components/tools/ToolsSidebarListItem.jsx
+++ src/components/tools/ToolsSidebarListItem.jsx
@@ -1,23 +1,29 @@
 import styles from "../sidebar/Sidebar.module.css";
+import { Emoji } from "emoji-picker-react";
 
 export default function ToolsSidebarListItem({tool}) {
 
     const voteOrVotes = tool.vote_count === 1 ? "Vote" : "Votes";
     
     return (
-        <div className={`${styles["sidebar-item"]} row g-0`}>
+        <a href={`/tools/${tool.slug}`}>
+            <div className={`${styles["sidebar-item"]} row g-0`}>
 
-            <div className={`${styles["sidebar-item-icon"]} col-2`}>
-                <div>
-                    <img src="https://via.placeholder.com/50" alt="..." />
+                <div className={`${styles["sidebar-item-icon"]} col-auto`}>
+                    <div id="tool-icon">
+                        <Emoji unified={tool.icon} size={36} />                    
+                    </div>
+                </div>  
+                <div className={`${styles["sidebar-item-desc"]} col ps-3`}>
+                    
+                        <div>
+                            <h4>{tool.title}</h4>
+                            <div className={`${styles["sidebar-item-desc-misc"]}`}>{tool.vote_count} {voteOrVotes}<span> &#183; </span>{tool.topics[0].title}<span> &#183; </span>{tool.created}</div>
+                        </div>
+                    
                 </div>
-            </div>  
-            <div className={`${styles["sidebar-item-desc"]} col-10 ps-2`}>
-                <div>
-                    <h4>{tool.title}</h4>
-                    <div className={`${styles["sidebar-item-desc-misc"]}`}>{tool.vote_count} {voteOrVotes}<span> &#183; </span>{tool.topics[0].title}<span> &#183; </span>{tool.created}</div>
-                </div>
+                
             </div>
-        </div>
+        </a>
     )
 }
